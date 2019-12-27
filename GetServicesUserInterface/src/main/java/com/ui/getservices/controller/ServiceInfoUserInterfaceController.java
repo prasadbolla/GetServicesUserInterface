121c8ea607c7ec4c33ef8df50b4bed620c63622c
Prasad Bolla <prasad.bolla1@ibm.com>
2019-12-27 13:15:26 -0500

    code changes for origin

--- GetServicesUserInterface/src/main/java/com/ui/getservices/controller/ServiceInfoUserInterfaceController.java
+++ GetServicesUserInterface/src/main/java/com/ui/getservices/controller/ServiceInfoUserInterfaceController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import com.ui.getservices.service.GetServicesImpl;
  *
  */
 @RestController
+@CrossOrigin
 public class ServiceInfoUserInterfaceController {
 	@Autowired
 	private GetServicesImpl getServicesImpl;
